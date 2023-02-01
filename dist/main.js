13c516978ff61b098227855e990889a1501ae79c
StasLis77 <u-stasa77@rambler.ru>
2023-02-01 14:22:32 +0300

    task 2

--- dist/main.js
+++ dist/main.js
@@ -7,26 +7,90 @@
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
 /******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
 /***/ "./src/index.js":
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-/***/ (() => {
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("document.write(\"Hello, world\");\r\n\r\n\r\n// import \"./style.css\";\r\n// import * as math from \"./math\";\r\n\r\n// const multiply = (a) => a * 8;\r\n\r\n// console.log(\"ES6 modules!\");\r\n// console.log(\"sum = \" + math.sum(2, 3));\r\n// console.log(\"multiply from index.js = \" + multiply(5));\r\n// console.log(\"multiply from math = \" + math.multiply(5));\n\n//# sourceURL=webpack://webpack-1/./src/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _math__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./math */ \"./src/math.js\");\ndocument.write(\"Hello, world\");\r\n\r\n\r\n// import \"./style.css\";\r\n\r\n\r\nconst multiply = (a) => a * 8;\r\n\r\nconsole.log(\"ES6 modules!\");\r\nconsole.log(\"sum = \" + _math__WEBPACK_IMPORTED_MODULE_0__.sum(2, 3));\r\nconsole.log(\"multiply from index.js = \" + multiply(5));\r\nconsole.log(\"multiply from math = \" + _math__WEBPACK_IMPORTED_MODULE_0__.multiply(5));\n\n//# sourceURL=webpack://webpack-1/./src/index.js?");
+
+/***/ }),
+
+/***/ "./src/math.js":
+/*!*********************!*\
+  !*** ./src/math.js ***!
+  \*********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"multiply\": () => (/* binding */ multiply),\n/* harmony export */   \"sum\": () => (/* binding */ sum)\n/* harmony export */ });\nconst sum = (a, b) => a + b; \r\nconst PI = 3.14;\r\n\r\nconst multiply = (a) => a * PI;\n\n//# sourceURL=webpack://webpack-1/./src/math.js?");
 
 /***/ })
 
 /******/ 	});
 /************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
 /******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/index.js");
 /******/ 	
 /******/ })()
 ;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ document.write("Hello, world");
 
 
 // import "./style.css";
-// import * as math from "./math";
+import * as math from "./math";
 
-// const multiply = (a) => a * 8;
+const multiply = (a) => a * 8;
 
-// console.log("ES6 modules!");
-// console.log("sum = " + math.sum(2, 3));
-// console.log("multiply from index.js = " + multiply(5));
-// console.log("multiply from math = " + math.multiply(5));
+console.log("ES6 modules!");
+console.log("sum = " + math.sum(2, 3));
+console.log("multiply from index.js = " + multiply(5));
+console.log("multiply from math = " + math.multiply(5));
